4d7b5f8cdf4da0da1026416f3266b1e5f429b887
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2025-02-18 05:12:42 -0800

    Update lib.rs

--- Diaverse/src/lib.rs
+++ Diaverse/src/lib.rs
@@ -1,14 +1,14 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub struct Shape {
+    pub x: i32,
+    pub y: i32,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub struct Storage {
+    pub location: &str,
+    pub max_chucks_shape: Shape,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub struct Chunk {
+    pub max_Shape: Shape,
+    pub atoms: Vec<Vec<f64>>,
 }
