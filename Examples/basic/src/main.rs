f275e5297796a02d6a04a567cb03bddc9c1f6cd9
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2025-02-21 09:32:15 -0800

    Update main.rs

--- Examples/basic/src/main.rs
+++ Examples/basic/src/main.rs
@@ -1,8 +1,11 @@
 use Diaverse::*;
 
 fn main() {
-    //window_init()
+    let mut (window, buffer) = window_init();
     let stored_world: Storage = Storage::create_new(Shape { x: 2, y: 2 }, "world/test.txt");
     generate_chunk(stored_world);
-    window_init();
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        window.update_with_buffer(&buffer, width, height).unwrap();
+    }
 }
